8ccffe956c10d1c01d29a42d9b6b0ec1a45ea7cf
Paul Fafengut <paul.fafengut93@gmail.com>
2022-10-28 22:20:32 +0200

    added linter, tailwind and removed HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import { useHead } from '@vueuse/head'
+  import AuctionData from '@/components/AuctionData.vue'
+
+  const whTooltips = {
+    colorLinks: true,
+    iconizeLinks: true,
+    renameLinks: true,
+  }
+  useHead({
+    script: [
+      {
+        src: 'https://wow.zamimg.com/js/tooltips.js',
+      },
+      {
+        textContent: `const whTooltips = {
+          colorLinks: true, 
+          iconizeLinks: true, 
+          renameLinks: true,
+          iconSize: 'large'
+        }`,
+      },
+    ],
+  })
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Saronite Shuffle</h1>
+  <p>
+    Saronite Shuffle is a strategy for turning as much profit as possible out of
+    Saronite Ore. The basic idea behind this is to prospect the Saronite Ore as
+    Jewelcrafter to acquire uncommon and rare Gems. With Proffesion like
+    Jewelcrafting, Alchemy and Enchanting you'll be able to turn these Gems into
+    other Items, that are worth way more then the Ore itself.
+  </p>
+  <p>
+    You can find a great Guide on WoWHead
+    <a href="https://www.wowhead.com/wotlk/guide/saronite-shuffle-gold-making"
+      >here</a
+    >
+  </p>
+  <AuctionData />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/AuctionData.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="flex">
+      <a href="https://www.wowhead.com/wotlk/item=36912/saronite-ore"></a>
+      <p>1.23g</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
